6855e294942417c413a4fbe69210ed399cc4cf49
Michael Aiyedun <michael.aiyedun@mavs.uta.edu>
2023-10-07 16:56:01 -0500

    updated activities

---
+++ app/src/main/java/com/example/techchex/Housing_Status_screen.java
@@ -0,0 +1,14 @@
+package com.example.techchex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Housing_Status_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_housing_status_screen);
+    }
+}
